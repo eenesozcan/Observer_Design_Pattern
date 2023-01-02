adad042ccbf3940a768327d75111b942dee83859
Enes OZCAN <enesozcan@hotmail.com>
2023-01-02 22:38:32 +0300

    Add project files.

---
+++ Observer_Design_Pattern/DAL/AppRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Observer_Design_Pattern.DAL
+{
+    public class AppRole:IdentityRole<int>
+    {
+
+    }
+}
---
+++ Observer_Design_Pattern/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Observer_Design_Pattern.DAL
+{
+    public class Context:IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-CSTSJL1\\MSSQLSERVER2019;initial catalog=ZObserver;integrated security=true");
+
+        }
+
+        public DbSet<Discount> Discounts { get; set; }
+    }
+}
